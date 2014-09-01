3c5503e0b3a356b2f5cf8e7e92aba41cb58106a6
Nelson Ramirez <nelson.ramirez@aboutobjects.com>
2014-09-01 18:28:00 -0500

    stubbed in a couple of tests to make sure setup is correct

---
+++ app/src/androidTest/java/com/studio1r/retrofitsandbox/apitests/VideoDetailTest.java
@@ -0,0 +1,21 @@
+package com.studio1r.retrofitsandbox.apitests;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by nelsonramirez on 9/1/14.
+ */
+public class VideoDetailTest extends AndroidTestCase {
+    public void testEndpointConfiguration() {
+        assertTrue(true);
+    }
+
+    public void testGetInvalidVideo() {
+        String id = "dummy";
+        assertEquals("dummy", "dummy");
+    }
+
+    public void testGetValidVideo() {
+        assertTrue(true);
+    }
+}
